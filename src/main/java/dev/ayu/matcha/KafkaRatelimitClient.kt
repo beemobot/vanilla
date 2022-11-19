9f25c635f4c551f612dc722799fe45eabcd79519
Adrian Paschkowski <git@wasdennnoch.me>
2022-11-19 09:57:01 +0100

    Support multiple ratelimit handlers for different clients (#8)
    * Support multiple ratelimit handlers for different clients
    
    * Update latte to main

--- src/main/java/dev/ayu/matcha/KafkaRatelimitClient.kt
+++ src/main/java/dev/ayu/matcha/KafkaRatelimitClient.kt
@@ -7,6 +7,8 @@ import dev.ayu.latte.logging.log
 import dev.ayu.latte.ratelimit.SharedRatelimitData
 import dev.ayu.latte.ratelimit.SharedRatelimitData.RatelimitClientData
 import dev.ayu.latte.util.SuspendingRatelimit
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
 // Give request expiry a bit of leeway in case of clock drift
@@ -18,35 +20,46 @@ class RatelimitClient(conn: KafkaConnection) : KafkaClient<RatelimitClientData>(
     SharedRatelimitData.RATELIMIT_TOPIC,
 ) {
 
-    private val globalRatelimit = SuspendingRatelimit(50, 1.seconds)
-    private val identifyRatelimit = SuspendingRatelimit(1, 5.seconds)
+    private val globalRatelimitProvider = RatelimitProvider(50, 1.seconds)
+    private val identifyRatelimitProvider = RatelimitProvider(1, 5.seconds)
 
     init {
         on(SharedRatelimitData.KEY_REQUEST_GLOBAL_QUOTA) { msg ->
-            handleRatelimitRequest(msg, globalRatelimit, "global")
+            handleRatelimitRequest(msg, globalRatelimitProvider, "global")
         }
 
         on(SharedRatelimitData.KEY_REQUEST_IDENTIFY_QUOTA) { msg ->
-            handleRatelimitRequest(msg, identifyRatelimit, "identify")
+            handleRatelimitRequest(msg, identifyRatelimitProvider, "identify")
         }
     }
 
     private suspend fun handleRatelimitRequest(
         msg: KafkaMessage<RatelimitClientData>,
-        limiter: SuspendingRatelimit,
+        ratelimitProvider: RatelimitProvider,
         type: String,
     ) {
         val sourceCluster = msg.headers.sourceCluster
         val expiresAt = msg.value?.requestExpiresAt
+        val client = msg.value?.client ?: "fallback"
         if (expiresAt != null && (expiresAt + EXPIRY_GRACE_PERIOD) < System.currentTimeMillis()) {
-            log.info("Incoming expired '$type' quota request from cluster $sourceCluster, ignoring")
+            log.info("Incoming expired '$type' quota request from client '$client' in cluster $sourceCluster, ignoring")
             // If the request has already expired, ignore it to not eat quotas unnecessarily
             return
         }
-        log.debug("Incoming '$type' quota request from cluster $sourceCluster")
-        limiter.requestQuota()
-        log.debug("Granted '$type' quota request for $sourceCluster")
+        log.debug("Incoming '$type' quota request from client '$client' in cluster $sourceCluster")
+        ratelimitProvider.getClientRatelimit(client).requestQuota()
+        log.debug("Granted '$type' quota request for client '$client' in cluster $sourceCluster")
         msg.respond(null)
+    }
+
+}
+
+private class RatelimitProvider(private val burst: Int, private val duration: Duration) {
+
+    private val limiters = ConcurrentHashMap<String, SuspendingRatelimit>();
+
+    fun getClientRatelimit(client: String): SuspendingRatelimit = limiters.computeIfAbsent(client) {
+        SuspendingRatelimit(burst, duration)
     }
 
 }
